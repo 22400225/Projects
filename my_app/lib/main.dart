50be5ac1b5948877087b1fd3458182842d5a449c
jiwonishere <namjiwon0216@gmail.com>
2025-08-16 00:26:07 +0900

    made setting

---
+++ my_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter'),
+        ),
+        body: const Center(
+          child: Text(
+            '안녕하세요! Flutter 예시입니다.',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
